b831b919fa73acebeea4917898a304f3b29f325a
0shadi <oshadiinduwari@gmail.com>
2025-05-27 03:03:16 +0530

    Added validations to billing details form

--- src/app/demo/pages/orders/billing-details-dialog/billing-details-dialog.component.ts
+++ src/app/demo/pages/orders/billing-details-dialog/billing-details-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -11,26 +11,30 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class BillingDetailsDialogComponent {
   billingDetailsForm : FormGroup;
   private dialogRef = inject(MatDialogRef<BillingDetailsDialogComponent>);
-
+  submitted = false;
 
   constructor(
     private fb : FormBuilder,
   ){
     this.billingDetailsForm= this.fb.group({
       orderId: new FormControl(''),
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      address: new FormControl(''),
-      district: new FormControl(''),
-      nearestCity: new FormControl(''),
-      postalCode: new FormControl(''),
-      contactNo: new FormControl(''),
-      email: new FormControl(''),
-      notes: new FormControl('')
+      firstName: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(15),Validators.pattern(/^[A-Za-z]+$/)]),
+      lastName: new FormControl('',[Validators.minLength(3),Validators.maxLength(15),Validators.pattern(/^[A-Za-z]+$/)]),
+      address: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(100)]),
+      district: new FormControl('',[Validators.required,Validators.pattern(/^[A-Za-z ]+$/)]),
+      nearestCity: new FormControl('',[Validators.pattern(/^[A-Za-z ]+$/)]),
+      postalCode: new FormControl('',[Validators.required,Validators.pattern(/^[0-9]{5}$/)]),
+      contactNo: new FormControl('',[Validators.required,Validators.pattern(/^0?[1-9]\d-?\d{7}$/)]),
+      email: new FormControl('',[Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
+      notes: new FormControl('',[Validators.maxLength(500)])
     });
   }
 
   onSubmit(){
+    this.submitted=true;
+    if(this.billingDetailsForm.invalid){
+        return;
+      }
     console.log('Billing details submitted:', this.billingDetailsForm.value);
     this.dialogRef.close(this.billingDetailsForm.value); // send data back
   }
--- src/app/demo/pages/orders/checkout/checkout.component.ts
+++ src/app/demo/pages/orders/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OnlineOrderingService } from 'src/app/services/online-ordering/online-ordering.service';
-import { FormBuilder,FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder,FormGroup, FormControl, Validators } from '@angular/forms';
 import { MessageServiceService } from 'src/app/services/message-service/message-service.service';
 import { CheckoutService } from 'src/app/services/checkout/checkout.service';
 
@@ -22,6 +22,7 @@ export class CheckoutComponent implements OnInit {
   dataSource:any[] = [];
   orderItems: any;
   selectedPaymentMethod:string;
+  submitted = false;
 
   constructor(
     private onlineOrderingService: OnlineOrderingService,
@@ -31,15 +32,15 @@ export class CheckoutComponent implements OnInit {
   ) {
     this.billingDetailsForm= this.fb.group({
       orderId: new FormControl(''),
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      address: new FormControl(''),
-      district: new FormControl(''),
-      nearestCity: new FormControl(''),
-      postalCode: new FormControl(''),
-      contactNo: new FormControl(''),
-      email: new FormControl(''),
-      notes: new FormControl('')
+      firstName: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(15),Validators.pattern(/^[A-Za-z]+$/)]),
+      lastName: new FormControl('',[Validators.minLength(3),Validators.maxLength(15),Validators.pattern(/^[A-Za-z]+$/)]),
+      address: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(100)]),
+      district: new FormControl('',[Validators.required,Validators.pattern(/^[A-Za-z ]+$/)]),
+      nearestCity: new FormControl('',[Validators.pattern(/^[A-Za-z ]+$/)]),
+      postalCode: new FormControl('',[Validators.required,Validators.pattern(/^[0-9]{5}$/)]),
+      contactNo: new FormControl('',[Validators.required,Validators.pattern(/^0?[1-9]\d-?\d{7}$/)]),
+      email: new FormControl('',[Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
+      notes: new FormControl('',[Validators.maxLength(500)])
     });
   }
 
@@ -62,6 +63,10 @@ export class CheckoutComponent implements OnInit {
   }
 
   placeOrder(){
+    this.submitted=true;
+    if(this.billingDetailsForm.invalid || !this.selectedPaymentMethod){
+        return;
+      }
     const totalCost = this.getTotalCost();
     const paymentMethod = this.selectedPaymentMethod;
     
