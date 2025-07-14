6b7f29f70ebeb54def7b321fdee6bae80e064db1
ChamithMendis <chamith109mendes@gmail.com>
2025-07-14 23:46:50 +0300

    customer login

--- src/app/demo/pages/login/customer-login/customer-login.component.ts
+++ src/app/demo/pages/login/customer-login/customer-login.component.ts
@@ -14,62 +14,62 @@ import { MessageServiceService } from 'src/app/services/message-service/message-
   styleUrl: './customer-login.component.scss'
 })
 export class CustomerLoginComponent implements OnInit {
-  customerLoginForm : FormGroup;
+  customerLoginForm: FormGroup;
 
-  displayedColumns: any[] = ['firstName','lastName','userName','actions'];
-  dataSource:MatTableDataSource<any>;
+  displayedColumns: any[] = ['customerName', 'userName', 'actions'];
+  dataSource: MatTableDataSource<any>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  submitted= false;
-  isDisabled =false;
+  submitted = false;
+  isDisabled = false;
   selectedData;
-  saveButtonLabel='Save';
+  saveButtonLabel = 'Save';
   mode = 'save';
 
   customers = [];
 
   constructor(
-    private fb : FormBuilder,
+    private fb: FormBuilder,
     private customerLoginService: CustomerLoginService,
     private messageService: MessageServiceService,
-    private customerRegistrationService: CustomerRegistrationService,
-  ){
-    this.customerLoginForm= this.fb.group({
-      id: new FormControl('',),
-      customerName : new FormControl(''),
-      firstName: new FormControl('', [Validators.required,Validators.pattern('^[A-Za-z]{2,30}$')]),
-      lastName: new FormControl('', [Validators.required,Validators.pattern('^[A-Za-z]{2,30}$')]),
-      userName: new FormControl('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._]{4,10}$')]),
-      password: new FormControl('', [Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=])[A-Za-z\\d!@#$%^&*()_+\\-=]{6,12}$')])
+    private customerRegistrationService: CustomerRegistrationService
+  ) {
+    this.customerLoginForm = this.fb.group({
+      id: new FormControl(''),
+      customerId: new FormControl(''),
+      customerName: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z]{2,30}$')]),
+      userName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._]{4,10}$')]),
+      password: new FormControl('', [Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=])[A-Za-z\\d!@#$%^&*()_+\\-=]{6,12}$')
+      ])
     });
-
   }
 
   ngOnInit(): void {
     this.populateData();
-    this.getCustomerNames();
+    this.getCustomers();
   }
 
   populateData() {
-    try{
+    try {
       this.customerLoginService.getCustomerLogin().subscribe({
-        next:(datalist:any[]) =>{
-        if(datalist.length <= 0){
-          return;
-        }
+        next: (datalist: any[]) => {
+          if (datalist.length <= 0) {
+            return;
+          }
 
-        console.log('get data response: ',datalist);
-        this.dataSource= new MatTableDataSource(datalist);
-  
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      },
-      error:(error) =>{
-        this.messageService.showError('Action failed with error ' + error);
-      }
-    });
-    }catch(error){
+          console.log('get data response: ', datalist);
+          this.dataSource = new MatTableDataSource(datalist);
+
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        },
+        error: (error) => {
+          this.messageService.showError('Action failed with error ' + error);
+        }
+      });
+    } catch (error) {
       this.messageService.showError('Action failed with error ' + error);
     }
   }
@@ -83,119 +83,123 @@ export class CustomerLoginComponent implements OnInit {
     }
   }
 
-  refreshData(){
+  refreshData() {
     this.populateData();
   }
 
-  onSubmit(){
-    this.submitted= true;
+  onSubmit() {
+    this.submitted = true;
 
-    try{
-      if(this.customerLoginForm.invalid){
-      return;
+    try {
+      if (this.customerLoginForm.invalid) {
+        return;
       }
 
-      if(this.mode === 'save'){
+      if (this.mode === 'save') {
         this.customerLoginService.serviceCall(this.customerLoginForm.value).subscribe({
-          next: (datalist:any[]) => {
-            if(datalist.length<=0){
+          next: (datalist: any[]) => {
+            if (datalist.length <= 0) {
               return;
             }
-            if(this.dataSource && this.dataSource.data && this.dataSource.data.length >0){
-              this.dataSource= new MatTableDataSource([datalist,...this.dataSource.data]);
+            if (this.dataSource && this.dataSource.data && this.dataSource.data.length > 0) {
+              this.dataSource = new MatTableDataSource([datalist, ...this.dataSource.data]);
+            } else {
+              this.dataSource = new MatTableDataSource([datalist]);
             }
-            
-            else{
-              this.dataSource= new MatTableDataSource([datalist]);
-            }
-            console.log("Login details submitted");
-          
-          this.messageService.showSuccess('Data Saved Successfully');
-        },
+            console.log('Login details submitted');
+
+            this.messageService.showSuccess('Data Saved Successfully');
+          },
           error: (error) => this.messageService.showError('Action failed with error ' + error)
         });
+      } else if (this.mode === 'edit') {
+        this.customerLoginService.editData(this.selectedData?.id, this.customerLoginForm.value).subscribe({
+          next: (datalist: any[]) => {
+            if (datalist.length <= 0) {
+              return;
+            }
+
+            let elementIndex = this.dataSource.data.findIndex((element) => element.id === this.selectedData?.id);
+            this.dataSource.data[elementIndex] = datalist;
+            this.dataSource = new MatTableDataSource(this.dataSource.data);
+
+            this.messageService.showSuccess('Data Edited Successfully');
+          },
+          error: (error) => this.messageService.showError('Action failed with error' + error)
+        });
       }
-    else if(this.mode === 'edit'){
-      this.customerLoginService.editData(this.selectedData?.id, this.customerLoginForm.value).subscribe({
-        next: (datalist:any[]) => {
-          if(datalist.length<=0){
-            return;
-          }
-
-          let elementIndex = this.dataSource.data.findIndex((element) => element.id === this.selectedData?.id);
-          this.dataSource.data[elementIndex] = datalist;          
-          this.dataSource = new MatTableDataSource(this.dataSource.data);
-
-          this.messageService.showSuccess('Data Edited Successfully');
-        },
-        error: (error) => this.messageService.showError('Action failed with error'+ error)
-      });
-    }
-    this.mode = 'save';
-    this.customerLoginForm.disable();
-    this.isDisabled = true;
-
-    }
-    catch(error){
+      this.mode = 'save';
+      this.customerLoginForm.disable();
+      this.isDisabled = true;
+    } catch (error) {
       this.messageService.showError('Action failed with error ' + error);
     }
-}
+  }
 
-resetData(){
-      this.saveButtonLabel = 'Save';
-      this.customerLoginForm.enable();
-      this.isDisabled= false;
-  
-      this.customerLoginForm.setErrors =null;
-      this.customerLoginForm.updateValueAndValidity();
-      this.submitted=false;
-    }
-  
-  editData(data:any){
+  resetData() {
+    this.saveButtonLabel = 'Save';
+    this.customerLoginForm.enable();
+    this.isDisabled = false;
+
+    this.customerLoginForm.setErrors = null;
+    this.customerLoginForm.updateValueAndValidity();
+    this.submitted = false;
+  }
+
+  editData(data: any) {
     this.customerLoginForm.patchValue(data);
     this.saveButtonLabel = 'Edit';
     this.mode = 'edit';
     this.selectedData = data;
   }
 
-  deleteData(data:any){
-    try{
+  deleteData(data: any) {
+    try {
       this.selectedData = data;
       const id = data.id;
 
-    this.customerLoginService.deleteData(id).subscribe({
-      next: (datalist:any[]) => {
-        if(datalist.length<=0){
-          return;
-        }
+      this.customerLoginService.deleteData(id).subscribe({
+        next: (datalist: any[]) => {
+          if (datalist.length <= 0) {
+            return;
+          }
 
-        const index = this.dataSource.data.findIndex((element) => element.id === id);
+          const index = this.dataSource.data.findIndex((element) => element.id === id);
 
-        if(index !== -1){//If the index is available
-          this.dataSource.data.splice(index,1); //Remove the item from the data source
-        }
+          if (index !== -1) {
+            //If the index is available
+            this.dataSource.data.splice(index, 1); //Remove the item from the data source
+          }
 
-        this.dataSource = new MatTableDataSource(this.dataSource.data);
+          this.dataSource = new MatTableDataSource(this.dataSource.data);
 
-        this.messageService.showSuccess('Data Deleted Successfully');
-      },
-      error: (error) => this.messageService.showError('Action failed with error'+ error)
-    });
-    }
-    catch(error){
+          this.messageService.showSuccess('Data Deleted Successfully');
+        },
+        error: (error) => this.messageService.showError('Action failed with error' + error)
+      });
+    } catch (error) {
       this.messageService.showError('Action failed with error ' + error);
     }
   }
 
-  getCustomerNames(){
+  getCustomers() {
     this.customerRegistrationService.getCustomer().subscribe({
-        next: (datalist:any[]) => {
-          if(datalist.length<=0){
-            return;
-          }
-          this.customers = datalist;
-        },        
-        error: (error) => this.messageService.showError('Action failed with error ' + error)
+      next: (datalist: any[]) => {
+        if (datalist.length <= 0) {
+          return;
+        }
+        this.customers = datalist;
+      },
+      error: (error) => this.messageService.showError('Action failed with error ' + error)
+    });
+  }
+
+  public onCustomerChange(data: any) {
+    const customerData = this.customers.find((cus: any) => cus.customerId = data);
+
+    this.customerLoginForm.patchValue({
+        customerId: data,
+        customerName: customerData.customerName,
       });
   }
 }
