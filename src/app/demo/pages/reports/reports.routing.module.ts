9357ac5d6bd0aad53ab031177cd9afe9113ad893
0shadi <oshadiinduwari@gmail.com>
2025-07-18 21:15:47 +0530

    report changes

--- src/app/demo/pages/reports/reports.routing.module.ts
+++ src/app/demo/pages/reports/reports.routing.module.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { EmployeeListComponent } from './Static Report/app/components/employee-list/employee-list.component';
 import { CustomerReportComponent } from './Static Report/app/components/customer-report/customer-report.component';
+import { MonthlySalesComponent } from './Charts/monthly-sales/monthly-sales.component';
+import { MonthlySalesIncomeComponent } from './Charts/monthly-sales-income/monthly-sales-income.component';
+import { WeeklyOrderStatusComponent } from './Charts/weekly-order-status/weekly-order-status.component';
+import { SalesPerItemComponent } from './Charts/sales-per-item/sales-per-item.component';
 
 export const ReportsRoute: Routes = [
   {
@@ -13,6 +17,22 @@ export const ReportsRoute: Routes = [
       {
         path: 'customer-report',
         component: CustomerReportComponent
+      },
+      {
+        path: 'monthly-sales-report',
+        component: MonthlySalesComponent
+      },
+      {
+        path: 'monthly-sales-income-report',
+        component: MonthlySalesIncomeComponent
+      },
+      {
+        path: 'weekly-order-status-report',
+        component: WeeklyOrderStatusComponent
+      },
+       {
+        path: 'sales-per-item-report',
+        component: SalesPerItemComponent
       }
     ]
   }
