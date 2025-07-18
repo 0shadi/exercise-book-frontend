33375750d1bf1fdf5f738d6ff11cdad3c74e5c3c
ChamithMendis <chamith109mendes@gmail.com>
2025-07-18 21:04:41 +0300

    customer login fixes

--- src/app/demo/pages/login/employee-login/employee-login.component.ts
+++ src/app/demo/pages/login/employee-login/employee-login.component.ts
@@ -9,6 +9,7 @@ import { MessageServiceService } from 'src/app/services/message-service/message-
 import { DeleteConfirmDialogComponent } from '../../registration/delete-confirm-dialog/delete-confirm-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { NotificationService } from 'src/app/services/notification-service/notification.service';
+import { RsaService } from 'src/app/services/rsa-service/rsa.service';
 
 @Component({
   selector: 'app-employee-login',
@@ -38,7 +39,8 @@ export class EmployeeLoginComponent implements OnInit {
     private employeeLoginService: EmployeeLoginService,
     private messageService: MessageServiceService,
     private employeeRegistrationService: EmployeeRegistrationServiceService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private rsaService:RsaService
   ) {
     this.employeeLoginForm = this.fb.group({
       id: new FormControl(''),
@@ -107,9 +109,22 @@ export class EmployeeLoginComponent implements OnInit {
       if (this.employeeLoginForm.invalid) {
         return;
       }
+      let  encryptedPassword = '';
+      if (this.employeeLoginForm.value.password) {
+        encryptedPassword = this.rsaService.encrypt(this.employeeLoginForm.value.password);
+      }
+
+      const loginFormCopy = {
+        id: this.employeeLoginForm.value.id,
+        employee: this.employeeLoginForm.getRawValue().employee,
+        firstName: this.employeeLoginForm.getRawValue().firstName,
+        lastName: this.employeeLoginForm.getRawValue().lastName,
+        userName: this.employeeLoginForm.getRawValue().userName,
+        password: encryptedPassword
+      };
 
       if (this.mode == 'save') {
-        this.employeeLoginService.serviceCall(this.employeeLoginForm.getRawValue()).subscribe({
+        this.employeeLoginService.serviceCall(loginFormCopy).subscribe({
           next: (datalist: any) => {
             if (!datalist) {
               return;
